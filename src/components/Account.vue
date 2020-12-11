e6aaac6b472f458b37d1e5ab2804bd69226740bc
akennedy <akbeast@rams.colostate.edu>
2020-12-10 22:49:00 -0700

    account page partially done

--- src/components/Account.vue
+++ src/components/Account.vue
@@ -1,11 +1,49 @@
 <template>
     <div>
         <h1> Account</h1>
+        <hr/>
+        <h3> {{firstName}}'s Reviews'</h3>
+
+        <table class="table">
+<thead>
+<th> Title </th>
+<th> Summary </th>
+<th> Turtles </th>
+
+</thead>
+
+            </table>
     </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
+    data(){
+return{
+    ReviewsByUser: null,
+    accountError: false
+}
+    },
+    computed: {
+        firstName(){ 
+            console.log(this.$store.state.user)
+            return this.$store.state.user.Name}
+    },
+    created(){
+        axios.get("/Customers_T/me", {
+            headers: {
+                Authorization: `Bearer ${this.$store.state.token}`
+
+        }
+        })
+        .then((response)=>{
+            console.log("here is /me response", response)
+            this.reviewsByUser = response.data
+
+        })
+
+    }
     
 }
 </script>
